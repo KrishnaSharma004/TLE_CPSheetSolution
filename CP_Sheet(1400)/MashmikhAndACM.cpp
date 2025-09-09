3da226220fb628be19d3d21ac43157ee9aab8ee3
Krishna Sharma <rakesh7355249204@gmail.com>
2025-09-09 22:45:38 +0530

    fff

---
+++ CP_Sheet(1400)/MashmikhAndACM.cpp
@@ -0,0 +1,176 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <cstring>
+#include <unordered_map>
+#include <deque>
+#include <numeric>
+#include <algorithm>
+#include <set>
+#include <queue>
+using namespace std;
+#define ll long long 
+#define pint pair<int,int>
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define vbool vector<bool>
+#define vtiii vector<tuple<int,int,int> >
+#define as(x) x.begin(),x.end()
+#define ds(y) y.rbegin(),y.rend()
+#define nl endl
+#define str string
+#define si set<int>
+#define ull unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mint map<int,int>
+#define fauto(i,x) for(auto &i:x)
+#define vpint vector<pair<int,int> >
+#define vpll vector<pair<ll,ll> >
+#define umpint unordered_map<int,int>
+#define vvint vector<vector<int> > 
+#define vvll vector<vector<ll> > 
+#define vpib vector<pair<int,bool> >
+#define maxelem(x) *max_element(x.begin(),x.end())
+#define minelem(x) *min_element(x.begin(),x.end())
+bool check(ll mid,vll &hg,ll x){
+    ll units=0;
+    int n=hg.size();
+    floop(i,0,n){
+        if(hg[i]<mid){
+            units += (mid-hg[i]);
+        }
+    }
+    return units<=x;
+}
+vint FindMax(vint &arr){
+    vpint tmp(arr.size());
+
+    floop(i,0,tmp.size()){
+        tmp[i].first=arr[i];
+        tmp[i].second=i;
+    }
+    sort(ds(tmp));
+    vint ans(3);
+    floop(i,0,3) ans[i]=tmp[i].second;
+    return ans;
+}
+ll calculatesum(ll start,ll end){
+    ll sum = ((start +end)*(end-start +1))/2;
+
+    return sum;
+}
+const ll mod = 1e9+7;
+bool pf(ll val,int ind,int x,int mid){
+    if(val + (1LL * (ind + 1)*(mid-1))<=x)
+        return true;
+    return false;
+}
+int binsearch(ll val,int ind,int x){
+    int low = 1,high=1e9+5;
+    int ans = 0;
+
+    while(low<=high){
+        int mid = (low + high)/2;
+        if(pf(val,ind,x,mid)){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return ans ;
+}
+int binarysearch(vll &pmax,int n,int val){
+    int low = 0,high=n-1;
+    int ans = -1;
+
+    while(low<=high){
+        int mid = (low + high)/2;
+        if(pmax[mid]<=val){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return ans ;
+}
+ll modl = 998244353;
+int ans=0;
+int dfs(int node,vvint &adj,str &s){
+    if(adj[node].size()==0){
+        return s[node-1]=='W'?1:-1;
+    }
+    int cnt = 0;
+    for(auto child:adj[node]){
+        cnt +=dfs(child,adj,s);
+    }
+    cnt += (s[node-1]=='W'?1:-1);
+    if(cnt==0) ans++;
+
+    return cnt;
+}
+ll calculateSumOfFirstX(int x){
+    return x*1ll*(x+1)/2;
+}
+const int MOD = 1000000007;
+void logic(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> factors[n + 1];
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j * j <= i; j++) 
+        {
+            if (i % j == 0)
+            {
+                factors[i].push_back(j);
+                if (i / j != j)
+                {
+                    factors[i].push_back(i / j);
+                }
+            }
+        }
+    }
+
+    int dp[k + 1][n + 1]; 
+
+    for (int i = 1; i <= n; i++)
+    {
+        dp[1][i] = 1;
+    }
+
+    for (int i = 2; i <= k; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            dp[i][j] = 0;
+            for (auto x : factors[j]) 
+            {
+                dp[i][j] = (dp[i][j] + dp[i - 1][x]) % MOD;
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        ans = (ans + dp[k][i]) % MOD;
+    }
+
+    cout << ans << endl;
+}
+int main(){
+    int t=1;
+    while(t--){
+        logic();
+    }
+    return 0;
+}
